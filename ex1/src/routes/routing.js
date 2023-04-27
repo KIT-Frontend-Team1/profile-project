a29b9a35180c71f96ecd30e88ac2870688779376
ooherin <555ohr@naver.com>
2023-04-28 00:14:00 +0900

    add:페이지 추가

--- ex1/src/routes/routing.js
+++ ex1/src/routes/routing.js
@@ -1,24 +1,28 @@
-import { createBrowserRouter } from 'react-router-dom'
-import Main from '../pages/main/Main'
-import AppProfile from '../pages/yeseul/AppProfile'
-
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../pages/main/Main";
+import AppProfile from "../pages/yeseul/AppProfile";
+import HyerinProfile from "../pages/hyerin/HyerinProfile";
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Main />
+    path: "/",
+    element: <Main />,
   },
   {
-    path: '/yeseul',
-    element: <AppProfile />
+    path: "/yeseul",
+    element: <AppProfile />,
   },
   {
-    path: '/seungyong',
-    element: <AppProfile />
+    path: "/seungyong",
+    element: <AppProfile />,
   },
   {
-    path: '/jaewon',
-    element: <AppProfile />
+    path: "/jaewon",
+    element: <AppProfile />,
   },
-])
+  {
+    path: "/hyerin/:id",
+    element: <HyerinProfile />,
+  },
+]);
 
-export default router
+export default router;
