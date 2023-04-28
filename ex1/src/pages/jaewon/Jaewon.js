9867e132b63259436192bd8814bf277f2701e5a2
Jaewon <grayhound0801@gmail.com>
2023-04-28 14:04:53 +0900

    재원 페이지 진행중

---
+++ ex1/src/pages/jaewon/Jaewon.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+const Jawon = () => {
+    return (
+        <>
+            <header>
+                <div style={} >
+
+                </div>
+            </header>
+        </>
+    );
+};
+
+export default Jawon;
--- ex1/src/routes/routing.js
+++ ex1/src/routes/routing.js
@@ -1,24 +1,25 @@
-import { createBrowserRouter } from 'react-router-dom'
-import Main from '../pages/main/Main'
-import AppProfile from '../pages/yeseul/AppProfile'
+import { createBrowserRouter } from 'react-router-dom';
+import Main from '../pages/main/Main';
+import AppProfile from '../pages/yeseul/AppProfile';
+import JawonHeader from '../components/JaewonHeader';
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Main />
-  },
-  {
-    path: '/yeseul',
-    element: <AppProfile />
-  },
-  {
-    path: '/seungyong',
-    element: <AppProfile />
-  },
-  {
-    path: '/jaewon',
-    element: <AppProfile />
-  },
-])
+    {
+        path: '/',
+        element: <Main />,
+    },
+    {
+        path: '/yeseul',
+        element: <AppProfile />,
+    },
+    {
+        path: '/seungyong',
+        element: <AppProfile />,
+    },
+    {
+        path: '/jaewon',
+        element: <JawonHeader />,
+    },
+]);
 
-export default router
+export default router;
